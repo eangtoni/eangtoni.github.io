586e7070c34faea56318daaabb8c78fd597f4d6e
eangtoni <erik.angtoni88@gmail.com>
2020-07-28 22:58:07 +0800

    Updates

---
+++ precache-manifest.477dff5cb74aa89b16b678bdaed04b10.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a12b3feaa03aed961d2e99e97d818c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0707f0ca66cd8bd47234",
+    "url": "/static/css/main.dbc51a20.chunk.css"
+  },
+  {
+    "revision": "9751aa795a0b327c055c",
+    "url": "/static/js/2.0fb63ae3.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.0fb63ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0707f0ca66cd8bd47234",
+    "url": "/static/js/main.190ed377.chunk.js"
+  },
+  {
+    "revision": "79c19766b9fafc23cac3",
+    "url": "/static/js/runtime-main.e387f811.js"
+  },
+  {
+    "revision": "676b68bd6e7091d79052707fec3d8803",
+    "url": "/static/media/rosetta1.676b68bd.png"
+  },
+  {
+    "revision": "08ba3dce2f5b49b088a465daa021c083",
+    "url": "/static/media/rosetta2.08ba3dce.png"
+  },
+  {
+    "revision": "37ca3b60c7563213506a2307c9b3e209",
+    "url": "/static/media/tulip1.37ca3b60.png"
+  },
+  {
+    "revision": "501cdcd7751f0761478f0cf571528060",
+    "url": "/static/media/tulip2.501cdcd7.png"
+  }
+]);
